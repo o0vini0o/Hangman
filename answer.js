760a699cfaaab77ee1c0ed4dfab1586c441a5da7
Yan Yang <y.y.erica@hotmail.com>
2025-09-04 14:01:24 +0200

    add answer container

---
+++ answer.js
@@ -0,0 +1,10 @@
+const answer = () => {
+  const answerContainer = document.getElementById("answer-container");
+  const question = "Pferd";
+  const answerChars = [...question];
+
+  answerChars.forEach((letters) => {
+    const charContainer = document.createElement("div");
+    charContainer.classList.add("letter-card");
+  });
+};
